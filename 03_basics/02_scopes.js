777898a54047f875d26f8268f202927f6c6fcaf0
Aman kumar tripathi <amankumartripathi1221@gmail.com>
2025-02-10 10:43:18 +0530

    JS Practice

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,45 @@
+// var c=300
+let a=300
+
+if(true){
+let a=10
+const b=20
+// var c=30
+// console.log("Inner: ",a);
+}
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+ 
+
+
+function one(){
+    const username="Aman"
+
+    function two(){
+        const website="youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+    two()
+    
+}
+
+// one()
+
+
+//************************interesting************************/
+
+console.log(addone(5))
+function addone(num){
+    return num+1
+}
+
+
+
+// addtwo(5)
+const addtwo=function(num){
+    return num+2
+}
