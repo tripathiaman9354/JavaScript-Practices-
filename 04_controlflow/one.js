783d4a52529e4324a3cbd203c8c70edc3b405e05
Aman kumar tripathi <amankumartripathi1221@gmail.com>
2025-02-10 16:42:50 +0530

    JS Practices

---
+++ 04_controlflow/one.js
@@ -0,0 +1,50 @@
+// if
+const isUserloggedIn=true
+const temp=41
+
+// if(temp<50){
+// console.log("temperature is less than 50");
+// }
+// else{
+//     console.log("temperature is greater than 50");
+// }
+
+
+// const score=200
+// // const power="aman"
+// if(score>100){
+//     const power="fly"
+//     console.log(`User power:${power}`);
+// }
+// console.log(`User power:${power}`);
+
+
+
+const balance=1000
+
+// if(balance>500) console.log("test"),console.log("test2");
+
+// if(balance<500){
+//     console.log("less than 500");
+// }else if(balance<750){
+//     console.log("less than 750");
+// }else if(balance<900){
+//     console.log("less than 900");
+// }else{
+//     console.log("less than 1200");
+// }
+
+
+const UserloggedIn=true
+const debitcard=true
+const loggedInFromGoogle=false
+const loggedInFromEmail=true
+if(UserloggedIn && debitcard && 2==2){
+    console.log("Allow to buy course");
+}
+
+if(loggedInFromEmail || loggedInFromGoogle){
+    console.log("User logged In");
+    
+}
+//,<,>,<=,>=,==,!=,===,!==
